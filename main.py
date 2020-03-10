1c8efa87f1916cd6215ba3da742a73344759b1b4
Anik Das <anikdas0811@gmail.com>
2020-03-10 23:20:23 +0530

    Removed bug which crashed game on clicking anywhere besides the squares
    Blitting circles still not working
    
    Co-Authored-By: Mr-DareDevil <mr-daredevil@users.noreply.github.com>

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def getIndex(x, y):
             return 3
         if 351 < y < 500:
             return 6
+        else:
+            return -1
     elif 151 < x < 299:
         if 50 < y < 199:
             return 1
@@ -23,6 +25,8 @@ def getIndex(x, y):
             return 4
         if 351 < y < 500:
             return 7
+        else:
+            return -1
     elif 301 < x < 450:
         if 50 < y < 199:
             return 2
@@ -30,11 +34,13 @@ def getIndex(x, y):
             return 5
         if 351 < y < 500:
             return 8
+        else:
+            return -1
     else:
         return -1
 
 
-board = [-1] * 9    # initialising the board with -1, which means no moves
+board = [-1] * 9  # initialising the board with -1, which means no moves
 
 # dictionary of index and render coordinate
 coord = {0: (75, 125),
@@ -50,13 +56,16 @@ coord = {0: (75, 125),
 
 # function to display cross and circle according to click coordinates and chance
 def display_sign(ind, sign):
-    if sign == 0:
-        screen.blit(circle, coord[ind])
-    if sign == 1:
-        screen.blit(cross, coord[ind])
+    if index != -1:
+        if sign == 0:
+            screen.blit(circle, coord[ind])
+        if sign == 1:
+            screen.blit(cross, coord[ind])
 
 
 running = True
+who = 0 # check if player will draw cross or circle
+
 while running:
     screen.fill((255, 253, 208))
 
@@ -78,8 +87,6 @@ while running:
             click_x = click_coord[0]
             click_y = click_coord[1]
             index = getIndex(click_x, click_y)
-            print(board)
-            print((index, event.pos))
-            display_sign(index, 0)
+            display_sign(index, who)
 
     pygame.display.update()
